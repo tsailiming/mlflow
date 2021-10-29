10d2e416387ed1329947858e898b9c5e2fb0ed8f
Tsai Li Ming <tsai.liming@gmail.com>
2021-10-30 00:55:26 +0800

    - Add MLflow 1.21.0 - Add nginx reverse proxy for basic auth - Add mlflow examples

---
+++ examples/samples/wine-quality.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export MLFLOW_S3_ENDPOINT_URL=https://s3-minio.apps.cluster-435b.435b.sandbox766.opentlc.com
+export MLFLOW_TRACKING_URI=https://mlflow-mlflow.apps.cluster-435b.435b.sandbox766.opentlc.com
+export AWS_ACCESS_KEY_ID=mlflow
+export AWS_SECRET_ACCESS_KEY=tT8FqTVT3EP8Fmgv
+export MLFLOW_TRACKING_USERNAME=mlflow
+export MLFLOW_TRACKING_PASSWORD=czZ7BmL7eKqsTcCm
+
+./wine-quality.py
