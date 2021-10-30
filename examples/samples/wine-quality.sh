862a439fe7f19cb60db2efa349b57c2d56836f78
Tsai Li Ming <tsai.liming@gmail.com>
2021-10-30 20:30:11 +0800

    Include info on how to change credentials

--- examples/samples/wine-quality.sh
+++ examples/samples/wine-quality.sh
@@ -2,8 +2,12 @@
 
 export MLFLOW_S3_ENDPOINT_URL=https://s3-minio.apps.cluster-435b.435b.sandbox766.opentlc.com
 export MLFLOW_TRACKING_URI=https://mlflow-mlflow.apps.cluster-435b.435b.sandbox766.opentlc.com
+
+# s3 bucket
 export AWS_ACCESS_KEY_ID=mlflow
 export AWS_SECRET_ACCESS_KEY=tT8FqTVT3EP8Fmgv
+
+# MLflow basic authentication
 export MLFLOW_TRACKING_USERNAME=mlflow
 export MLFLOW_TRACKING_PASSWORD=czZ7BmL7eKqsTcCm
 
